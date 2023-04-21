afad8d6a930e144646fb8ac969027a95fb30cd03
Iván <ivantbd87@gmail.com>
2023-04-21 21:15:52 +0200

    feat: created delete post button, no funcional

--- frontend-cleverpy/src/components/Card.tsx
+++ frontend-cleverpy/src/components/Card.tsx
@@ -11,7 +11,11 @@ export const Card = (props: Post) => {
     <div className="d-flex flex-column bg-dark text-light rounded shadow px-3 post-card">
       <h4 className="text-decoration-underline text-center mt-2">{props.title}</h4>
       <p className="mt-3 text-center ">{props.body}</p>
-      <small className="d-block text-end mb-2 mt-auto">Created by user: {props.userId}</small>
+      <div className="d-flex mt-auto mb-2 justify-content-between">
+        <button className="btn btn-sm btn-outline-danger">Delete</button>
+        <small className="d-block text-end mb-2 fw-lighter">Created by user: {props.userId}</small>
+      </div>
+      {/* <button className="btn btn-sm btn-outline-secondary">Edit post</button> */}      
     </div>
   )
 }
