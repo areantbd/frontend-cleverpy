59d97bb0a858bdaa8ffe648fc4111a8fb44e80fe
Iván <ivantbd87@gmail.com>
2023-04-22 07:03:06 +0200

    feat: delete posts done

--- frontend-cleverpy/src/components/Card.tsx
+++ frontend-cleverpy/src/components/Card.tsx
@@ -3,18 +3,15 @@ interface Post {
   id: number,
   userId: number,
   title: string,
-  body: string
+  body: string,
 }
 
 export const Card = (props: Post) => {
   return (
     <div className="d-flex flex-column bg-dark text-light rounded shadow px-3 post-card">
-      <h4 className="text-decoration-underline text-center mt-2">{props.title}</h4>
-      <p className="mt-3 text-center ">{props.body}</p>
-      <div className="d-flex mt-auto mb-2 justify-content-between">
-        <button className="btn btn-sm btn-outline-danger">Delete</button>
-        <small className="d-block text-end mb-2 fw-lighter">Created by user: {props.userId}</small>
-      </div>
+      <h4 className="text-decoration-underline text-center mt-5">{props.title}</h4>
+      <p className="mt-3 text-center text-truncate">{props.body}</p>
+        <small className="d-block text-end mb-2 fw-lighter mt-auto ms-auto" >Created by user: {props.userId}</small>
       {/* <button className="btn btn-sm btn-outline-secondary">Edit post</button> */}      
     </div>
   )
