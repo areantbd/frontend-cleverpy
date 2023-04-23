a83361ff17f7fe8dce7e03e54e5eddbe92724333
Iván <ivantbd87@gmail.com>
2023-04-23 11:55:27 +0200

    first tests with user context

--- frontend-cleverpy/src/main.tsx
+++ frontend-cleverpy/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx'
 import "./sass/style.scss"
 import Navbar from './components/ui/Navbar.tsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import UserContextProvider from './components/contexts/UserContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Navbar />
-      <App />
+      <UserContextProvider>
+        <Navbar />
+        <App />
+      </UserContextProvider>
     </Router>
   </React.StrictMode>,
 )
