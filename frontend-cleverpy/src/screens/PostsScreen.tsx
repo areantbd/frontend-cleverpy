a6db8cf7ba57f582d84f0f7fd3d44dca6828714e
Iván <ivantbd87@gmail.com>
2023-04-25 23:20:58 +0200

    fix: fixed trouble with brranch merge

--- frontend-cleverpy/src/screens/PostsScreen.tsx
+++ frontend-cleverpy/src/screens/PostsScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Card } from "../components/Card"
+import { Card } from "../components/card/Card"
 import axios from "axios"
 import { Link } from "react-router-dom"
 
